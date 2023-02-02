27a06bae553c9167fac00de82411335d36acabff
wambui-nyambura <nyamburawambuui@gmail.com>
2023-02-02 16:35:39 +0300

    one commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+   var fullname = "joyce"
+    var age = 18
+    var phonenumber = 374783883
+    var weight = 50.7
+    var citizen = "Kenya"
+    var isKenyan = true
+    var isUgandan = false
+}
